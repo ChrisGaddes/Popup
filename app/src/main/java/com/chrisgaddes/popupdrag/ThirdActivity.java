4792926a6e07225c5180e76190a413f22d10643f
Chris Gaddes <cagaddes@gmail.com>
2016-06-14 10:21:41 -0500

    Added arrow head

--- app/src/main/java/com/chrisgaddes/popupdrag/ThirdActivity.java
+++ app/src/main/java/com/chrisgaddes/popupdrag/ThirdActivity.java
@@ -5,11 +5,9 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
-import android.graphics.Rect;
 import android.os.Bundle;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 
@@ -21,53 +19,58 @@ public class ThirdActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        CrazyEightsView ev = new CrazyEightsView(this);
+        ArrowView ev = new ArrowView(this);
 
         setContentView(ev);
     }
 
-    public class CrazyEightsView extends View {
+    public class ArrowView extends View {
 
         private Paint redPaint;
         private Paint blackPaint;
         private int circleX;
         private int circleY;
-        private int dim_square;
-        private int left_coord, top_coord, right_coord, bottom_coord;
         private int btn_loc_x;
         private int btn_loc_y;
         private double length_arrow;
-        private Double angle;
+        private double angle;
+
+        private double len_arrow_head;
+        private float x3;
+        float x4;
+        float y3;
+        float y4;
+
+        private double arrow_head_left;
+        private double arrow_head_right;
+        float loc_arrow_head_left_x;
+        float loc_arrow_head_left_y;
+        float loc_arrow_head_right_x;
+        float loc_arrow_head_right_y;
+
 
         double pi = Math.PI;
         double angles[] = {-pi, -3 * pi / 4, -pi / 2, -pi / 4, 0, pi / 4, pi / 2, 3 * pi / 4, pi, 2 * pi};
 
-        public CrazyEightsView(Context context) {
+        public ArrowView(Context context) {
             super(context);
             redPaint = new Paint();
             redPaint.setAntiAlias(true);
             redPaint.setColor(Color.rgb(230, 00, 00));
-            redPaint.setStrokeWidth(10f); //TODO change from absolute to dependant on
+            redPaint.setStrokeWidth(10f);
 
             blackPaint = new Paint();
-
 
             btn_loc_x = 700;
             btn_loc_y = 700;
             circleX = btn_loc_x;
             circleY = btn_loc_y;
-
-
-            dim_square = 200;
-
-
-            left_coord = circleX;// - dim_square / 2;
-            top_coord = btn_loc_y - dim_square / 2;
-            right_coord = btn_loc_x + dim_square / 2;
-            bottom_coord = btn_loc_y + dim_square / 2;
-            Rect rectangle = new Rect(left_coord, top_coord, right_coord, bottom_coord);
-
-            Log.d(TAG, "Rectangle Coordinates" + rectangle);
+            loc_arrow_head_left_x = btn_loc_x;
+            loc_arrow_head_left_y = btn_loc_y;
+            loc_arrow_head_right_x = btn_loc_x;
+            loc_arrow_head_right_y = btn_loc_y;
+            length_arrow = 300;
+            len_arrow_head = 100;
 
 //http://stackoverflow.com/questions/16559244/rectangle-is-not-drawn-at-its-original-coordinates
 
@@ -78,23 +81,50 @@ public class ThirdActivity extends AppCompatActivity {
 
         public void onDraw(Canvas canvas) {
             super.onDraw(canvas);
-            Paint paint = new Paint();
+
+
+            Paint paint_green_line = new Paint();
             Path path = new Path();
-            paint.setStyle(Paint.Style.FILL);
-            paint.setColor(Color.TRANSPARENT);
-            canvas.drawPaint(paint);
+            paint_green_line.setStyle(Paint.Style.FILL);
+            paint_green_line.setColor(Color.TRANSPARENT);
+            canvas.drawPaint(paint_green_line);
             path.moveTo(btn_loc_x, btn_loc_y);
             path.lineTo(circleX, circleY);
+
+
             path.close();
-            paint.setStrokeWidth(20f);
-            paint.setPathEffect(null);
-            paint.setColor(Color.GREEN);
-            paint.setStyle(Paint.Style.STROKE);
-            canvas.drawPath(path, paint);
+            paint_green_line.setStrokeWidth(20f);
+            paint_green_line.setPathEffect(null);
+            paint_green_line.setColor(Color.GREEN);
+            paint_green_line.setStyle(Paint.Style.STROKE);
+            canvas.drawPath(path, paint_green_line);
+
+            Paint paint_arrow_head = new Paint();
+            Path path_arrow_head = new Path();
+            paint_arrow_head.setStyle(Paint.Style.FILL);
+            paint_arrow_head.setColor(Color.TRANSPARENT);
+            canvas.drawPaint(paint_arrow_head);
+
+            path_arrow_head.moveTo(circleX, circleY);
+            path_arrow_head.lineTo(loc_arrow_head_left_x, loc_arrow_head_left_y);
+
+            path_arrow_head.moveTo(circleX, circleY);
+            path_arrow_head.lineTo(loc_arrow_head_right_x, loc_arrow_head_right_y);
+
+            path_arrow_head.close();
+            paint_arrow_head.setStrokeWidth(20f);
+            paint_arrow_head.setPathEffect(null);
+            paint_arrow_head.setColor(Color.GREEN);
+            paint_arrow_head.setStyle(Paint.Style.STROKE);
+            canvas.drawPath(path_arrow_head, paint_arrow_head);
 
 
             canvas.drawCircle(btn_loc_x, btn_loc_y, 50, blackPaint);
             //canvas.drawLine(btn_loc_x, btn_loc_y, circleX, circleY, redPaint);
+
+//            canvas.drawLine(btn_loc_x, btn_loc_y, circleX, circleY, paint_arrow_head);
+//            canvas.drawLine(circleX, circleY, x3, y3, paint_arrow_head);
+//            canvas.drawLine(circleX, circleY, x4, y4, paint_arrow_head);
 
         }
 
@@ -110,13 +140,22 @@ public class ThirdActivity extends AppCompatActivity {
                     circleX = X;
                     circleY = Y;
 
+                    angle = Math.atan2(circleY - btn_loc_y, circleX - btn_loc_x);
+                    arrow_head_left = 4 * pi / 3 - angle;
+                    arrow_head_right = -pi / 3 - angle;
+
+                    loc_arrow_head_left_x = (float) ((float) len_arrow_head * Math.sin(arrow_head_left) + circleX);
+                    loc_arrow_head_left_y = (float) ((float) len_arrow_head * Math.cos(arrow_head_left) + circleY);
+
+                    loc_arrow_head_right_x = (float) ((float) len_arrow_head * Math.sin(arrow_head_right) + circleX);
+                    loc_arrow_head_right_y = (float) ((float) len_arrow_head * Math.cos(arrow_head_right) + circleY);
+
                     invalidate();// call invalidate to refresh the draw
                     break;
                 case MotionEvent.ACTION_UP:
                     // snap to another position
                     circleX = X;
                     circleY = Y;
-                    length_arrow = 300;
                     angle = Math.atan2(circleY - btn_loc_y, circleX - btn_loc_x);
 
 
@@ -131,17 +170,21 @@ public class ThirdActivity extends AppCompatActivity {
                     }
                     double angle = angles[idx];
 
-                    Log.d(TAG, "Angle snap = " + angle);
-
                     double angle_degrees = Math.toDegrees(-angle);
-
-                    Snackbar.make(this, "Created force at " + angle_degrees , Snackbar.LENGTH_SHORT).show();
+                    Snackbar.make(this, "Created force at " + angle_degrees, Snackbar.LENGTH_SHORT).show();
 
                     circleY = (int) (length_arrow * Math.sin(angle) + btn_loc_x);
                     circleX = (int) (length_arrow * Math.cos(angle) + btn_loc_y);
 
-                    invalidate();
+                    arrow_head_left = 4 * pi / 3 - angle;
+                    arrow_head_right = -pi / 3 - angle;
 
+                    loc_arrow_head_left_x = (float) ((float) len_arrow_head * Math.sin(arrow_head_left) + circleX);
+                    loc_arrow_head_left_y = (float) ((float) len_arrow_head * Math.cos(arrow_head_left) + circleY);
+                    loc_arrow_head_right_x = (float) ((float) len_arrow_head * Math.sin(arrow_head_right) + circleX);
+                    loc_arrow_head_right_y = (float) ((float) len_arrow_head * Math.cos(arrow_head_right) + circleY);
+
+                    invalidate();
 
                     break;
 
